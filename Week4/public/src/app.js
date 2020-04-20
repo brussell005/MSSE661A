a72a1088f440e836b261b82a62bb4bc798086c44
Brien Russell <brussell@Briens-iMac.hsd1.al.comcast.net>
2020-04-19 20:08:27 -0500

    Finally adjusted the code to work with my database and tables

--- Week4/public/src/app.js
+++ Week4/public/src/app.js
@@ -10,21 +10,13 @@ const doLogin = function(e) {
       window.location.href = 'home.html';
     });
   };
-  
+  //
   const doRegister = function(e) {
     e.preventDefault();
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-  
-    register({
-      username: username,
-      email: email,
-      password: password
-    }).then(function(res) {
-      window.location.href = 'home.html';
-    });
-  };
+ 
   
   const doLogout = function(e) {
     e.preventDefault();
---
+++ week5done/week5mine/public/src/inventory.service.js
@@ -0,0 +1,3 @@
+const inventory_API = `${BASE_API_URL}/inventory`; // http://localhost:3000/api/inventory
+
+const getinventory = () => _get(inventory_API);
