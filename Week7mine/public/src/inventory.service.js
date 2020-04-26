70582745ef65df8260a0aae60cf25c9428407277
Brien Russell <brussell@Briens-iMac.hsd1.al.comcast.net>
2020-04-25 20:13:23 -0500

    Update for Week7 with test coverage

--- Week7mine/public/src/inventory.service.js
+++
@@ -1,9 +0,0 @@
-const inventory_API = `${BASE_API_URL}/inventory`; // http://localhost:3000/api/inventory
-
-const getItems = () => _get(inventory_API, OPTIONS_WITH_AUTH);
-
-const AddItem = (formData) => 
-  _post(inventory_API, formData, DEFAULT_OPTIONS_WITH_AUTH);
-
-const deleteItem = (itemId) =>
-  _delete(`${inventory_API}/${itemId}`, OPTIONS_WITH_AUTH);
